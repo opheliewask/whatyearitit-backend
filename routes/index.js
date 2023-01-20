fcd44bb8c796e1d943b3e233c1d2258eeaf2b988
Ophelie <opheliewask@yahoo.fr>
2023-01-20 15:45:40 +0100

    done2 ajout de year a la place de date

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/date', (req, res) => {
+router.get('/year', (req, res) => {
   const date = new Date();
   res.json({ year: date.getFullYear() });
  });
